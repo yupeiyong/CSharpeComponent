fb1fa2a0524cb3b37372bdbe00ea50602061a68f
yupeiyong <2758318132@qq.com>
2018-08-22 23:46:18 +0800

    add interface files.

---
+++ CSharpeComponents.Auth/IAuthFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpeComponents.Auth
+{
+    public interface IAuthFilter
+    {
+        bool CanAccess(object userId, string url);
+    }
+}
---
+++ CSharpeComponents.Auth/IAuthProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpeComponents.Auth
+{
+    public interface IAuthProvider
+    {
+        /// <summary>
+        /// 无需登录就可以访问的地址
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetFreeAccessUrls();
+
+        /// <summary>
+        /// 登录后即可访问的地址
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetDefaultAccessUrls();
+
+
+        /// <summary>
+        /// 用户信息关键字
+        /// </summary>
+        /// <returns></returns>
+        string GetUserInfoKey();
+
+
+        /// <summary>
+        /// 登录页面
+        /// </summary>
+        /// <returns></returns>
+        string GetLoginPageUrl();
+
+
+        /// <summary>
+        /// 没有权限时，跳转的页面
+        /// </summary>
+        /// <returns></returns>
+        string GetAccessDenyPageUrl();
+
+        List<IAuth> GetUserAuths(object userId);
+
+        List<IRoleAuth> GetUserRoles(object userId);
+    }
+}
